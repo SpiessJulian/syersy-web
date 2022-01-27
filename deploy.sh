95c0ae301b1fdfb192ad64cb1841f7c8ecd86d36
David Julian Spiess <spiessapps@gmail.com>
2022-01-27 12:28:45 -0300

    Reducing to 1 job in CI

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t julianspiess/syersy-web:"$HASH" ./app
-docker push julianspiess/syersy-web:"$HASH"
+docker build -t julianspiess/syersy-web:"$CIRCLE_SHA1" ./app
+docker push julianspiess/syersy-web:"$CIRCLE_SHA1"
 
 # Deploying in production.
-echo "$SSH_KEY" | base64 --decode > id_rsa.private
-chmod 600 id_rsa.private
-echo "" >> "$HOME/.ssh/known_hosts"
-ssh -t -i id_rsa.private "$DEPLOY_USERNAME"@syersy.com "$DEPLOY_CMD"
+echo "syersy.com" >> "$HOME/.ssh/known_hosts"
+ssh -t "$DEPLOY_USERNAME"@syersy.com "$DEPLOY_CMD"
