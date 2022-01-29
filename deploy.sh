034d6b6c1e129b2ebcba7eea5b1054d123b1538b
David Julian Spiess <spiessapps@gmail.com>
2022-01-29 12:52:48 -0300

    Add selective deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t julianspiess/syersy-web:"$CIRCLE_SHA1" ./app
-docker push julianspiess/syersy-web:"$CIRCLE_SHA1"
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker build -t julianspiess/syersy-web:"$CIRCLE_SHA1" ./app
+    docker push julianspiess/syersy-web:"$CIRCLE_SHA1"
 
-# Deploying in production.
-echo "syersy.com" >> "$HOME/.ssh/known_hosts"
-ssh -t "$DEPLOY_USERNAME"@syersy.com "$DEPLOY_CMD"
+    # Deploying in production.
+    echo "$DEPLOY_HOST" >> "$HOME/.ssh/known_hosts"
+    echo "[+] Deploying (env=production, branch=master)"
+    ssh -t "$DEPLOY_USERNAME"@"$DEPLOY_HOST" "$DEPLOY_CMD"
+elif [ "$CIRCLE_BRANCH" == "staging" ]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker build -t julianspiess/syersy-web:"$CIRCLE_SHA1" ./app
+    docker push julianspiess/syersy-web:"$CIRCLE_SHA1"
+
+    # Deploying in staging.
+    echo "$DEPLOY_HOST" >> "$HOME/.ssh/known_hosts"
+    echo "[+] Deploying (env=staging, branch=staging)"
+    ssh -t "$DEPLOY_USERNAME"@"$DEPLOY_HOST" "$DEPLOY_CMD_STAGING"
+fi
